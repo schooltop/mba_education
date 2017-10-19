0f0c68620269c34bb5ce0d5ec2de9e3f14a0a5b1
jamst <jamstjamst@163.com>
2017-10-19 15:54:25 +0800

    add migrate

--- db/migrate/20171018063318_created_student_classes.rb
+++ db/migrate/20171018063318_created_student_classes.rb
@@ -1,4 +1,10 @@
 class CreatedStudentClasses < ActiveRecord::Migration[5.1]
   def change
+  	t.string  :name
+  	t.string  :name_en
+  	t.string  :name_short
+  	t.integer  :as_type
+  	t.text    :descripe
+    t.timestamps	
   end
 end
--- db/migrate/20171018063336_created_courses.rb
+++ db/migrate/20171018063336_created_courses.rb
@@ -1,4 +1,16 @@
 class CreatedCourses < ActiveRecord::Migration[5.1]
   def change
+  	t.string  :name
+  	t.string  :name_en
+  	t.string  :name_short
+  	t.string  :base_object
+  	t.integer :parent_id
+  	t.integer :as_type
+  	t.float   :hours
+  	t.float   :weight
+  	t.float   :credit
+  	t.text    :descripe
+  	t.text    :descripe_en
+    t.timestamps
   end
 end
